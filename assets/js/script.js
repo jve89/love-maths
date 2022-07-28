f1c90852cf88ffa6d8eab95c87ddd202b2b51045
jve89 <johanvanerkel@gmail.com>
2022-07-28 17:44:11 +0000

    trying some code for the challange. Without succes. Tomorrow continue

--- assets/js/script.js
+++ assets/js/script.js
@@ -37,6 +37,7 @@ function runGame(gameType) {
     // Creates 2 random numbers between 1 and 25
     let num1 = Math.floor(Math.random() * 25) +1;
     let num2 = Math.floor(Math.random() * 25) +1;
+    let numMulti = num1 * num2;
 
     if (gameType === "addition") {
         displayAdditionQuestion(num1, num2);
@@ -44,6 +45,8 @@ function runGame(gameType) {
         displayMultiplyQuestion(num1, num2);
     } else if (gameType === "subtract") {
         displaySubtractQuestion(num1, num2);
+    } else if (gameType === "division") {
+        displayDivisionQuestion(numMulti, num2);
     } else {
         alert(`Unknown game type: ${gameType} `);
         throw `Unknown game type: ${gameType}. Aborting!`;
@@ -86,6 +89,8 @@ function calculateCorrectAnswer() {
         return[operand1 * operand2, "multiply"];
     } else if (operator === '-') {
         return[operand1 - operand2, "subtract"];
+    } else if (operator === '/') {
+        return[operand1 / operand2, "division"];
     } else {
         alert(`Unimplemented operator ${operator}`);
         throw `Unimplemented operator ${operator}. Aborting!`;
@@ -131,6 +136,8 @@ function displayMultiplyQuestion(operand1, operand2) {
     document.getElementById('operator').textContent = "x";
 }
 
-function displayDivisionQuestion() {
-
+function displayDivisionQuestion(operand1, operand2) {
+    document.getElementById('operand1').textcontent = operand1 > operand2 ? operand1 : operand2;
+    document.getElementById('operand2').textcontent = operand1 > operand2 ? operand2 : operand1;
+    document.getElementById('operator').textContent = "/";
 }
